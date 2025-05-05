87fce3d60218e09f0962d90aef3e0abe10e7fcc7
Sridhar Yadavilli <yadavillisridhar@gmail.com>
2025-05-05 22:14:05 +0530

    Initial commit

---
+++ src/main/java/com/surfbird/SpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.surfbird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+
+}
